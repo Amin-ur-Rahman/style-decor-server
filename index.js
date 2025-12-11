ccd5fcd68db2543660779213412afc3c72dfb95e
Amin-ur-Rahman <aminurar679799@gmail.com>
2025-12-11 13:42:24 +0600

    add service by admin and api to send services data in client side

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ const runDB = async () => {
     );
 
     const usersColl = db.collection("users");
+    const serviceColl = db.collection("services");
 
+    // USERS collection api
     app.post("/users", async (req, res) => {
       try {
         const userData = req.body;
@@ -67,6 +69,35 @@ const runDB = async () => {
         res.status(404).send({ message: "data not found" });
       }
     });
+
+    // SERVICE related api
+
+    app.post("/services", async (req, res) => {
+      const serviceData = req.body;
+      try {
+        const result = await serviceColl.insertOne(serviceData);
+        if (!result.insertedId) {
+          return res.send({ message: "failed to add service" });
+        }
+        res.send(result);
+        console.log(result);
+      } catch (error) {
+        res.status(500).send({ message: "server internal error" });
+        console.error(error);
+      }
+    });
+
+    app.get("/services", async (req, res) => {
+      try {
+        const result = await serviceColl.find().toArray();
+        return res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "server internal error" });
+        console.error(error);
+      }
+    });
+
+    // closing here--------------
   } catch (err) {
     console.error(err);
   }
